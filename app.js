4b72b312bf50d8dc1b73783ce84d7c02e5865f7a
ragav <ragavkutti777@gmail.com>
2018-03-10 23:37:45 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var express=require("express");
+var app=express();
+var bodyparser=require("body-parser");
+
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
+var data=200;
+
+app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
+
+
+io.on("connection",function(socket)
+{
+    console.log("A user connected:" + socket.id);
+
+    socket.join("room01-ehealth");
+
+    socket.emit("notify",{'message':"Welcome to car parking service"});
+
+    socket.on('alert',function(data){
+        io.to("room01-ehealth").emit("alert_m",{'message':"Alert triggered manually!!please go for medical checkup"});
+    });
+});
+
+
+
+app.get('/',function(req,res){
+    res.send("<marquee>car parking is online</marquee>");
+});
+app.get('/high',function(req,res){
+    console.log("high");
+    io.to("room01-ehealth").emit('notify',{'message':"Alert:parking at 1"});
+    res.send("done");
+});
+
+app.get('/Alert',function(req,res){
+    console.log("Manual Alert received");
+    io.to("room01-ehealth").emit('alert_m',{'message':"parking is full"});
+    res.send("done");
+});
+
+app.get('/value',function(req,res){
+    console.log(req.query.storage);
+    data=req.query.storage;
+    io.to("room01-ehealth").emit('rk',{'message':data});
+    res.send("done");
+});
+
+app.get('/low',function(req,res){
+    console.log("low");
+    io.to("room01-ehealth").emit('notify',{'message':"Alert: parking at 2"});
+    res.send("done");
+});
+
+
+
+
+var server=http.listen(process.env.PORT || 5000,function(){
+    console.log("server running in port "+(process.env.PORT || 5000));
+});
+
